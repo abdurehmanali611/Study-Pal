e82b3274e3d7fdc05f30ea47eb396a285d1ae45a
Abdurehman Ali <abdurehmanali611@g-mail.com>
2023-11-25 12:57:35 +0300

    menu_and_profile

--- Front_Pages/Home.js
+++ Front_Pages/Home.js
@@ -1,10 +1,16 @@
-import { View, Text, Image, StyleSheet, FlatList, Alert, ScrollView, TouchableOpacity} from 'react-native'
+import { View, Text, 
+  Image, StyleSheet, 
+  ScrollView, TouchableOpacity, TextInput} 
+  from 'react-native'
 import React, { useState } from 'react'
 
 const Home = () =>{
 
   const [menubtn, setMenubtn] = useState(true)
+  const [profilebtn, setProfilebtn] = useState(true)
+  const [profiles, setProfiles] = useState(false)
   const [pages, setPages] = useState(false)
+
   const menupressed = () => {
 
       setMenubtn(!menubtn)
@@ -12,16 +18,41 @@ const Home = () =>{
 
   }
 
+  const profilepressed = () => {
+
+    setProfiles(!profiles)
+    setProfilebtn(!profilebtn)
+
+  }
+
   return <ScrollView>
     <View>
     <View style = {styles.header_images}>
-        <View>
-        <TouchableOpacity>
-        <Image 
-        source={require('../assets/profile.png')}
-        style = {styles.profile}
-        />
-        </TouchableOpacity>
+        <View style = {styles.profileoption}> 
+        {profiles ? (
+          <View>
+            <TouchableOpacity onPress={profilepressed}>
+            <Image 
+            source={require('../assets/back.jpg')}
+            style = {styles.back}
+            />
+          </TouchableOpacity>
+          <View style = {styles.profileelement}>
+          <TouchableOpacity>
+            <Image
+            source={require('../assets/profile.png')}
+            style = {styles.back}
+            />
+          </TouchableOpacity>
+          </View>
+          </View>
+        ):
+        <TouchableOpacity onPress={profilepressed}>
+          <Image 
+          source={require('../assets/profile.png')}
+          style = {styles.profile}
+          />
+          </TouchableOpacity>}
         </View>
         <View>
           {menubtn ? (
@@ -33,24 +64,32 @@ const Home = () =>{
             </TouchableOpacity>
           ):
            pages && (
-            <View>
+            <View style = {styles.menuoption}>
               <TouchableOpacity onPress={menupressed}>
                <Image 
                 source={require('../assets/close.jpg')}
                 style = {styles.menuimage}
                />
               </TouchableOpacity>
-              <TouchableOpacity>
-                <Text>Software Engineering</Text>
+              <TouchableOpacity 
+              style = {styles.menuelement}
+              >
+                <Text style = {styles.textmenu}>Software Engineering</Text>
               </TouchableOpacity>
-              <TouchableOpacity>
-                <Text>Computer Science</Text>
+              <TouchableOpacity
+              style = {styles.menuelement}
+              >
+                <Text style = {styles.textmenu}>Computer Science</Text>
               </TouchableOpacity>
-              <TouchableOpacity>
-                <Text>Infomation Technology</Text>
+              <TouchableOpacity
+              style = {styles.menuelement}
+              >
+                <Text style = {styles.textmenu}>Infomation Technology</Text>
               </TouchableOpacity>
-              <TouchableOpacity>
-                <Text>Information System</Text>
+              <TouchableOpacity
+              style = {styles.menuelement}
+              >
+                <Text style = {styles.textmenu}>Information System</Text>
               </TouchableOpacity>
               </View>
            )
@@ -90,16 +129,46 @@ const styles = StyleSheet.create ({
         marginHorizontal: 20, 
         marginVertical: 20
     },
+    menuoption: {
+      position: 'absolute',
+      zIndex: 2
+    },
+    profileelement: {
+      marginHorizontal: 50
+    },
+    profileoption: {
+      position: 'absolute',
+      zIndex: 2
+    },
+    menuelement: {
+      backgroundColor: '#7ED7C1',
+      height: 70,
+      borderRadius: 20,
+      width: 280,
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginHorizontal: 80
+    },
+    textmenu: {
+      fontSize: 17,
+      fontStyle: 'italic'
+    },
+    back: {
+      width: 60,
+      height: 50,
+      marginTop: 20,
+      marginHorizontal: 10,
+      borderRadius: 30,
+    },
     header_images: {
       display: 'flex',
       flexDirection: 'row'
     },
     menuimage: {
-      marginHorizontal: 220,
+      marginHorizontal: 320,
       marginVertical: 20,
       width: 50,
       height: 50,
-      
     },
     titleparent: {
       marginTop: 10
